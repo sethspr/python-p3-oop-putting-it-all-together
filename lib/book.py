ceb66596140c8aec71494f206ce6386a67827615
seth spring <seth.c.spring@gmail.com>
2024-03-13 16:24:38 -0400

    complete with notes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    #test 1
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def __repr__(self):
+         return f"<Book: {self.title}, Pages: {self._page_count}>"
+    #test 2
+    @property
+    def page_count(self):
+        return self._page_count
     
-        
+    @page_count.setter
+    def page_count(self, page_count_int):
+        if type(page_count_int) == int:
+            self._page_count = page_count_int
+        print("page_count must be an integer")
+
+    #to convert a string into an integer. this works, but it's not what the test is looking for. 
+    def int_page_count(self): 
+        page_int = int(self.page_count)
+        print(type(page_int))
+        return page_int
+        # for this to work, on line 9, 'Pages:' needs to be {self.int_page_count()}
+    
+    #test 3
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+my_book = Book("And Then There Were None", 272)
+
+print(my_book)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = None #default value of None becuase we do not have any info on the shoe condition. 
+
+    def __repr__(self):
+        return f"<Brand: {self.brand}, Size: {self._size}>"
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size_int):
+        if type(size_int) == int:
+            self._size = size_int
+        print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+    # When the cobble method is called, it sets the condition to "New", indicating that the shoe has been repaired and is now in a new condition.
+
+   
+my_shoe = Shoe("Adidas", 9)
+
+print(my_shoe)
